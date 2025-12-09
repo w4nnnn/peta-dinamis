b0436983c38c276e8b8bdf6bfec507729e989e83
wans112 <irwanfebriansyah112@gmail.com>
2025-12-09 18:13:13 +0700

    feat: Tambahkan konfigurasi PM2 untuk aplikasi peta-dinamis

---
+++ ecosystem.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+    apps: [
+        {
+            name: "peta-dinamis",
+            script: "npm",
+            args: "start",
+            env: {
+                NODE_ENV: "production",
+            },
+        },
+    ],
+};
